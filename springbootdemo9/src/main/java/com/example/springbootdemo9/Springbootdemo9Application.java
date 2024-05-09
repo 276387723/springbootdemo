eb3f940687277c192c343ca9417a0b4510f2860d
276387723 <168957521+276387723@users.noreply.github.com>
2024-05-09 15:16:57 +0800

    Add files via upload

---
+++ springbootdemo9/src/main/java/com/example/springbootdemo9/Springbootdemo9Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootdemo9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootdemo9Application.class, args);
+    }
+
+}
---
+++ springbootdemo9/src/test/java/com/example/springbootdemo9/Springbootdemo9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootdemo9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
